c15c3668f72b43c7017dca322a59084d4835cad6
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-04-18 14:48:07 +0200

    Update role.middleware response

--- src/app/auth/role.middleware.ts
+++ src/app/auth/role.middleware.ts
@@ -11,7 +11,7 @@ export function roleCheck() {
     if (userRole === 'READER' && req.method.toUpperCase() !== 'GET') {
 
       log.warn('auth.role_check_failure', { user });
-      res.status(403).json({ error: 'You are not authorized to perform this operation' });
+      res.status(403).json({ msg: 'You are not authorized to perform this operation' });
       return next('Unauthorized');
 
     } else {
