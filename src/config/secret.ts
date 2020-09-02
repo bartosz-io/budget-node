4fcbb095c333b80588eea8fc69b7fdd757f3dba0
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-09-02 13:21:26 +0200

    Add Google secret config placeholder

--- src/config/secret.ts
+++ src/config/secret.ts
@@ -2,5 +2,9 @@ export const secret = {
   github: {
     clientID: 'ABC', // place `clientID` here
     clientSecret: 'ABC' // place `clientSecret` here
+  },
+  google: {
+    clientID: 'ABC', // place `clientID` here
+    clientSecret: 'ABC' // place `clientSecret` here
   }
 }
