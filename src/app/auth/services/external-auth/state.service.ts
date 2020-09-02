803670b6e8f2aaad99f30ea398cd8e3f005b45a7
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-09-02 12:17:55 +0200

    Add state service for external auth

---
+++ src/app/auth/services/external-auth/state.service.ts
@@ -0,0 +1,31 @@
+const randtoken = require('rand-token');
+import log from '../../../../utils/logger';
+
+const LENGTH = 32;
+
+export default {
+
+  setAndGetNewState(session: any) {
+    session.oauthState = randtoken.generate(LENGTH);
+    return session.oauthState;
+  },
+
+  getAndRemoveState(session: any) {
+    const state = session.oauthState;
+    session.oauthState = null;
+    return state;
+  },
+
+  assertStateIsValid(session: any, state: string) {
+    return new Promise((resolve, reject) => {
+      if (!!state && state.length === LENGTH && state === session.oauthState) {
+        log.info('auth.external.state.valid_check', { state });
+        resolve();
+      } else {
+        log.error('auth.external.state.failed_check', { state: state, expectedState: session.oauthState });
+        reject('Invalid state paramater');
+      }
+    });
+  }
+
+}
