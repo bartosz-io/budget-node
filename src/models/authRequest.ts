954a042a0231cecf50e484c609b745561d57bbf1
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-09-17 16:27:21 +0200

    Add `otp` field to AuthRequest

--- src/models/authRequest.ts
+++ src/models/authRequest.ts
@@ -2,10 +2,18 @@ import { Request } from 'express';
 
 export class AuthRequest {
     
-    constructor(public email: string, public password: string, public session?: any) { }
+    constructor(
+        public email: string, 
+        public password: string,
+        public otp?: string,
+        public session?: any) { }
 
     static buildFromRequest(req: Request): AuthRequest {
-        return new AuthRequest(req.body.email, req.body.password, req.session);
+        return new AuthRequest(
+            req.body.email,
+            req.body.password,
+            req.body.otp,
+            req.session);
     }
 
 }
