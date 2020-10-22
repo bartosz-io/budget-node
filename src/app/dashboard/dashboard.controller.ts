ad515ef3b0d3cb61d0f3a583ae69a33d4f302430
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-10-22 15:19:59 +0200

    Change path to query params in dashboard controller

--- src/app/dashboard/dashboard.controller.ts
+++ src/app/dashboard/dashboard.controller.ts
@@ -7,14 +7,14 @@ import { User } from '../../models/user';
 const router = Router();
 const repository: BudgetRepository = new InMemoryBudgetRepository(); // TODO Use DI container
 
-router.get('/budgets/:month/:year', function (req: Request, res: Response) {
+router.get('/budgets', function (req: Request, res: Response) {
   const user = req.user as User;
   repository.getBugdets(user.accountId as string, buildPeriodFromRequest(req))
     .then(budgets => res.json(budgets))
     .catch(() => res.status(404).json({msg: 'Budgets not found'}));
 });
 
-router.get('/budget-summary/:month/:year', function (req: Request, res: Response) {
+router.get('/budget-summary', function (req: Request, res: Response) {
   const user = req.user as User;
   repository.getBudgetSummary(user.accountId as string, buildPeriodFromRequest(req))
     .then(summary => res.json(summary))
--- src/utils/controller.utils.ts
+++ src/utils/controller.utils.ts
@@ -2,7 +2,7 @@ import { Request } from 'express';
 import { Period } from '../models/period';
 
 export function buildPeriodFromRequest(req: Request) {
-    const month = parseInt(req.params.month);
-    const year = parseInt(req.params.year);
-    return new Period(month, year);
-  }
+  const month = parseInt(req.query.month);
+  const year = parseInt(req.query.year);
+  return new Period(month, year);
+}
