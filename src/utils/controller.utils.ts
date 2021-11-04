10b76eb47cc4b2475b747679b1a68c160d830f5a
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2021-11-04 21:58:20 +0100

    Update types

--- src/utils/controller.utils.ts
+++ src/utils/controller.utils.ts
@@ -2,7 +2,7 @@ import { Request } from 'express';
 import { Period } from '../models/period';
 
 export function buildPeriodFromRequest(req: Request) {
-  const month = parseInt(req.query.month);
-  const year = parseInt(req.query.year);
+  const month = parseInt(req.query.month as string);
+  const year = parseInt(req.query.year as string);
   return new Period(month, year);
 }
